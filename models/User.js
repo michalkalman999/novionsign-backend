ffbefecd42f0b7dcd27a89ef02194ce694e3915e
michalkalman999 <michal.kalman@novion-pharma.com>
2025-08-05 14:15:18 +0200

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters']
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
