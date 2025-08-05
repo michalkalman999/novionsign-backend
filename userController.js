f672cec490cd09b11221a4735e713e16a892f34e
michalkalman999 <michal.kalman@novion-pharma.com>
2025-08-05 11:09:43 +0200

    controllers/userController.js

---
+++ userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
